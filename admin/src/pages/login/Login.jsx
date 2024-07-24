8e6c983a47249848b5f0b87635070a4d08f93f3a
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2024-07-24 22:38:28 +0530

    test login

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -25,6 +25,7 @@ const Login = () => {
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post('auth/login', credentials);
+      console.log(res)
       if (res.data.isAdmin) {
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
 
