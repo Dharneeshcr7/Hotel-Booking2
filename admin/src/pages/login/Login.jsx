f95a02f9439d1ba4771570d1d77d75e58219a044
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2024-07-24 23:28:10 +0530

    c1

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -24,11 +24,11 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post('auth/login', credentials);
+      const res = await axios.post('https://hotel-booking2-17hc.onrender.com/api/auth/login', credentials);
       console.log(res)
       if (res.data.isAdmin) {
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
-
+        
         navigate("/");
       } else {
         dispatch({
