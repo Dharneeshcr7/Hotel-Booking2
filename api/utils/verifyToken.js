9ca5042d934b20ced5263fa640893d285858126a
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2024-07-25 23:03:41 +0530

    change noew

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token || req.header["Auth"];
+  const token = req.cookies.access_token || req.headers["Auth"];
   console.log(token)
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
