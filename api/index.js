5d40650dc83f8073b7b3381015ad44119b42e4d1
Dharneeshcr7 <76260467+Dharneeshcr7@users.noreply.github.com>
2024-07-27 01:00:26 +0530

    domain client added

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 
 const corsOptions = {
-  origin: ['http://localhost:3000','https://hotel-booking2-iota.vercel.app/','https://hotel-booking2-1.onrender.com'],
+  origin: ['http://localhost:3000','https://hotel-booking2-iota.vercel.app/','https://hotel-booking2-1.onrender.com','https://hotel-booking2-2.onrender.com/'],
   credentials: true
   
 };
